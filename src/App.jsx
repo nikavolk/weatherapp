576c869de11cb5de8ddb178e087312776a6b9d16
Nika V <76059172+nikavolk@users.noreply.github.com>
2023-02-21 15:17:43 +0100

    Add setIsLoading to error msg

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
         }
 
         setError("Enter a valid place name");
+        setIsLoading(false);
         throw new Error("Enter a valid place name");
       })
       .then((data) => {
